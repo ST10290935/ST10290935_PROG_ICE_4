1aca5ae7a87a30e294d6aeeb51e28958cb23763e
ST10290935 <128598477+ST10290935@users.noreply.github.com>
2024-05-10 09:13:11 +0200

    Add files via upload

---
+++ Quantity/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Quantity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ideas idk = new ideas();
+            while (true)
+            {
+                Console.WriteLine("select the following:\n1)add number \n2)Scale number \n3)Revert numver \n4)Click For Fun\n5)Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+               
+                switch (choice) {
+                    case 1:
+                        idk.Quans();
+                        break;
+                        case 2:
+                        idk.Scale();
+                        break;
+                        case 3:
+                        idk.Revert();
+                            break;
+                    case 4:
+                        idk.Crazy();
+                        break;
+                        case 5:
+                        Environment.Exit(0);
+                            break;
+                        //default:
+                        //Console.WriteLine("invalid input");
+                        //break;
+
+                }
+                
+
+            }
+        }
+    }
+}
---
+++ Quantity/ideas.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Quantity
+{
+    internal class ideas
+
+
+    {
+
+        public int quantity;
+        public int originalQuan;
+        public ArrayList stuffs = new ArrayList();
+        public ArrayList stuffs2 = new ArrayList();
+
+        public void Quans() {
+            Console.WriteLine("enter a value");
+            quantity = Convert.ToInt32(Console.ReadLine());
+            originalQuan = quantity;
+            stuffs.Add(quantity);
+            
+            stuffs2.Add(originalQuan);
+
+        }
+        public void Scale() {
+           
+            Console.WriteLine("select a scale option:\n1)0.5 \n2)2 \n3)3");
+            int choose = Convert.ToInt32(Console.ReadLine());
+            switch (choose) {
+                case 1:
+                    double scale = quantity * 0.5;
+                    Console.WriteLine("This is the scaled amount: " + scale);
+                    break;
+                    case 2:
+                    double scale1 = quantity * 2;
+                    Console.WriteLine("This is the scaled amount: " + scale1);
+                    break;
+                    case 3:
+                    double scale3 = quantity * 3;
+                    Console.WriteLine("This is the scaled amount: " + scale3);
+                    break;
+
+            }
+        }
+        public void Revert()
+        {
+            int revert = originalQuan;
+            Console.WriteLine("this is the orignal value entered: " + originalQuan);
+        }
+        public void Crazy() {
+           
+
+            for (int i = 1; i <= quantity; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(quantity);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
